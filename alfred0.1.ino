f1fe5e69df881e38f6893702ce1f9284635affa7
alexjones9 <36506100+alexjones9@users.noreply.github.com>
2018-02-18 20:58:20 +0000

    Add files via upload

---
+++ alfred0.1.ino
@@ -0,0 +1,110 @@
+int sensorPin = A0;            
+unsigned int sensorValue = 0;  
+float voltage;
+
+#define ECHOPIN1 2
+#define TRIGPIN1 3
+#define ECHOPIN2 4
+#define TRIGPIN2 5
+#define ROOM1 6
+#define ROOM2 7
+#define ROOM3 8
+#define ALARM 10
+
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(TRIGPIN1, OUTPUT);
+  pinMode(ECHOPIN1, INPUT);
+  pinMode(TRIGPIN2, OUTPUT);
+  pinMode(ECHOPIN2, INPUT);
+  pinMode(ALARM,OUTPUT);
+  //pinmode everything
+  
+  
+}
+
+
+ 
+void DoorCam(){
+  //if doorbell = high
+    //Serial.print("Doorbell is being rang")
+  //send to website
+  
+  
+}
+
+
+
+
+void MotionDetection(int TRIGPIN, int ECHOPIN, int ROOM) {
+  float duration, distance;
+  int movement;
+  digitalWrite(TRIGPIN, LOW); 
+  delayMicroseconds(2);
+ 
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);
+  
+  duration = pulseIn(ECHOPIN, HIGH);
+  distance = (duration / 2) * 0.0344;
+  
+  if (distance >= 200){ //range could be calculated at setup so room size doesn't affect?
+    ++movement;
+  }
+  else {
+    movement = 0;
+    digitalWrite(ROOM, HIGH);
+  }
+
+ if (movement = 50) {  //placeholder value
+  digitalWrite(ROOM, LOW);
+ }
+
+}
+
+void HouseAlarm (int TRIGPIN, int ECHOPIN){
+  float duration, distance;
+  int movement;
+  bool turnoff;
+  digitalWrite(TRIGPIN, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);
+  
+  duration = pulseIn(ECHOPIN, HIGH);
+  distance = (duration / 2) * 0.0344;
+  
+  if (distance <= 200){ //range could be calculated at setup so room size doesn't affect?
+    tone(ALARM, 300);
+  }
+   
+  turnoff = Serial.read(); //comman to turn off from website or keypad maybe?
+  if (turnoff = true) {
+    noTone(ALARM); 
+ }
+
+}
+
+
+void WakeUpAlarm(){
+  // get real time
+  //Alarm = (ReceiveTime)
+  //CurrentTime
+  //if RecieveTime = CurrentTime
+  //tone(BUZZ,400);
+  //noTone (30);
+}
+
+void loop(){
+  MotionDetection(TRIGPIN1,ECHOPIN1,ROOM1);
+  MotionDetection(TRIGPIN2,ECHOPIN2,ROOM2);
+  HouseAlarm(TRIGPIN1, ECHOPIN1);
+  HouseAlarm(TRIGPIN2, ECHOPIN2);
+  
+ }
+
+
